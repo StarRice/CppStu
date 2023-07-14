f4736d9da7472b22f92b18602944b7efe9a84bd3
yw o <1622945822@qq.com>
2023-07-14 17:28:11 +0800

    修复找不到标准库声明的问题

--- chapter_3/eg3_2.cpp
+++ chapter_3/eg3_2.cpp
@@ -2,7 +2,7 @@
  * @Author: yw o 1622945822@qq.com
  * @Date: 2023-07-13 14:45:25
  * @LastEditors: yw o 1622945822@qq.com
- * @LastEditTime: 2023-07-14 16:37:58
+ * @LastEditTime: 2023-07-14 17:28:08
  * @FilePath: \c++\hello.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -53,3 +53,4 @@ int main()
 #endif
     return 0;
 }
+//asd
